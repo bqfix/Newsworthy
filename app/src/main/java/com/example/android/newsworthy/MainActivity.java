49c2c3fa6dee3ecaa0fd9226ad61e31a02a7bd04
bqfix <benjaminfix@gmail.com>
2018-11-18 16:22:47 -0500

    Setup menu buttons
    To be edited later

--- app/src/main/java/com/example/android/newsworthy/MainActivity.java
+++ app/src/main/java/com/example/android/newsworthy/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.android.newsworthy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Temporary toast for testing clicks
+    Toast mTestToast = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    //Setup Menu bar actions for main activity
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //Click logic for menu
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Compare clicked item to the ID for existing buttons and execute custom logic for each and return true(action handled)
+        switch (item.getItemId()) {
+            case (R.id.action_search):
+                //TODO Add proper logic for buttons
+
+                //Toast Click Test
+                if (mTestToast != null) {
+                    mTestToast.cancel();
+                }
+                mTestToast = Toast.makeText(this, "Search", Toast.LENGTH_SHORT);
+                mTestToast.show();
+                return true;
+
+        }
+
+        //If no match, let the system handle the click with default behavior
+        return super.onOptionsItemSelected(item);
     }
 }
