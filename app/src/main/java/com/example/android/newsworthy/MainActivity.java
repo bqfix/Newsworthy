163be518a8a23479a7e9ce70eb762b5383643d58
bqfix <benjaminfix@gmail.com>
2018-11-18 18:06:17 -0500

    Helper methods
    Assigned variables, added helper methods to choose the visible view, and also noticed I forgot to give the error textview an id

--- app/src/main/java/com/example/android/newsworthy/MainActivity.java
+++ app/src/main/java/com/example/android/newsworthy/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.android.newsworthy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +20,27 @@ public class MainActivity extends AppCompatActivity {
     //Temporary toast for testing clicks
     Toast mTestToast = null;
 
+    //View member variables
+    LinearLayout mSearchLayout;
+    EditText mSearchText;
+    Button mExecuteSearch;
+    RecyclerView mResultsRecycler;
+    ProgressBar mProgressIndicator;
+    TextView mErrorText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign variables
+        mSearchLayout = (LinearLayout) findViewById(R.id.search_layout);
+        mSearchText = (EditText) findViewById(R.id.search_edit_text);
+        mExecuteSearch = (Button) findViewById(R.id.execute_search_button);
+        mResultsRecycler = (RecyclerView) findViewById(R.id.results_recycler_view);
+        mProgressIndicator = (ProgressBar) findViewById(R.id.progress_indicator);
+        mErrorText = (TextView) findViewById(R.id.error_text);
 
 
     }
@@ -37,13 +61,8 @@ public class MainActivity extends AppCompatActivity {
         //Compare clicked item to the ID for existing buttons and execute custom logic for each and return true(action handled)
         switch (item.getItemId()) {
             case (R.id.action_search):
-                //TODO Add proper logic for buttons
-
-                if (mTestToast != null) {
-                    mTestToast.cancel();
-                }
-                mTestToast = Toast.makeText(this, "Search", Toast.LENGTH_SHORT);
-                mTestToast.show();
+                //Search EditText and button should be invisible until this button is clicked
+                mSearchLayout.setVisibility(View.VISIBLE);
                 return true;
             case (R.id.action_preferences):
                 Intent prefIntent = new Intent(MainActivity.this, PreferencesActivity.class);
@@ -54,5 +73,24 @@ public class MainActivity extends AppCompatActivity {
 
         //If no match, let the system handle the click with default behavior
         return super.onOptionsItemSelected(item);
+    }
+
+    //Helper methods for showing results recycler view, progress bar, and error text
+    void showResults() {
+        mErrorText.setVisibility(View.INVISIBLE);
+        mProgressIndicator.setVisibility(View.INVISIBLE);
+        mResultsRecycler.setVisibility(View.VISIBLE);
+    }
+
+    void showProgress() {
+        mErrorText.setVisibility(View.INVISIBLE);
+        mResultsRecycler.setVisibility(View.INVISIBLE);
+        mProgressIndicator.setVisibility(View.VISIBLE);
+    }
+
+    void showErrorText() {
+        mProgressIndicator.setVisibility(View.INVISIBLE);
+        mResultsRecycler.setVisibility(View.INVISIBLE);
+        mErrorText.setVisibility(View.VISIBLE);
     }
 }
